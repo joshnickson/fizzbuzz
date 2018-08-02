25fbcccc976a3f76aafdba240ebded3c33a6513e
jnickson1506@gmail.com <ec2-user@ip-172-31-17-43.us-east-2.compute.internal>
2018-08-02 12:44:23 +0000

    final commit

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,21 @@
 require "fizzbuzz"
 describe 'fizzbuzz' do
-    it 'returns \"fizz\" when passed 3' do
+    it 'returns \"fizz\" when mulitple of 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
+    end
+    it 'returns \"fizz\" when mulitple of 3' do
+        expect(fizzbuzz(6)).to eq 'fizz'
+    end
+    it 'returns \"buzz\" when mulitple of 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+    it 'returns \"buzz\" when mulitple of 5' do
+        expect(fizzbuzz(10)).to eq 'buzz'
+    end
+    it 'returns \"buzz\" when multiple of 3 and 5' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+     it 'returns \"buzz\" when multiple of 3 and 5' do
+        expect(fizzbuzz(60)).to eq 'fizzbuzz'
     end
 end
